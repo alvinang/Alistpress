b77ee49f811998c58042e973b2b2bcf905fb0ccc
Alvin Ang <alvinangbs@gmail.com>
2014-03-19 14:35:11 -0700

    Update routes and controller action for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,18 @@
 Alistpress::Application.routes.draw do
   root to: 'home#index'  
   
-  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'new' }
+  authenticated do
+    root to: 'member_areas#index', as: :authenticated_root
+  end
+  
+  devise_for :users, 
+              path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'new' },  
+              skip: :sessions
+  
+  devise_scope :user do
+    match '/login', via: :get, to: 'devise/sessions#new', as: :new_user_session
+    match '/login', via: :post,  to: 'devise/sessions#create', as: :user_session
+    match '/logout', via: :delete, to: 'devise/sessions#destroy', as: :destroy_user_session  
+  end
+  
 end
