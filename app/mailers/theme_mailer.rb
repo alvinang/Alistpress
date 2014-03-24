28bb408ee12655db08be2a1fc180d9a3e727e187
Alvin Ang <alvinangbs@gmail.com>
2014-03-23 23:19:47 -0700

    Update mailer

--- app/mailers/theme_mailer.rb
+++ app/mailers/theme_mailer.rb
@@ -1,14 +1,9 @@
 class ThemeMailer < ActionMailer::Base
-  include ThemeMailer::Premailer::HtmlToPlainText
   
-  def send_email(theme)
-    debugger
+  def send_email(theme)    
     @theme = theme
     mail(to: @theme.recipient_email, from: @theme.sender_email, 
-          subject: @theme.title) do |format|
-          format.html { render @theme }
-          format.text { render convert_to_text(@theme.content)}
-        end          
+          subject: @theme.title)
     
   end
 end
