8f376284025a1fd81efea49b3e3ef6a719f8d0c9
Alvin Ang <alvinangbs@gmail.com>
2014-03-25 15:13:00 -0700

    Refactor js on home view

--- app/assets/javascripts/views/home/home.js
+++ app/assets/javascripts/views/home/home.js
@@ -3,7 +3,6 @@ window.Alistpress.Views.DashboardHome = Backbone.View.extend({
   template: JST['home/home'],
   
   render: function() {
-    debugger
     var renderedContent = this.template({
       atemplates: this.collection,
       sentThemes: Alistpress.themes.countSent()
