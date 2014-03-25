738d1fe18b970cb7c03c552111ca3d436bd266d7
Alvin Ang <alvinangbs@gmail.com>
2014-03-25 01:29:54 -0700

    Style dashboard pages

--- app/assets/javascripts/views/atemplates/index.js
+++ app/assets/javascripts/views/atemplates/index.js
@@ -11,10 +11,29 @@ window.Alistpress.Views.AtemplatesIndex = Backbone.View.extend({
   events: {
     "click button.index-delete-theme": "deleteTheme"
   },
+  
+  getCategories: function() {
+    var atemplates = this.collection;
+    
+    var result = [];
+    var categoriesAry = [];
+    _.each(atemplates.models, function(atemplate){
+      categoriesAry.push(atemplate.attributes.category);
+    });
+    
+    _.each(categoriesAry, function(category){
+      if (result.indexOf(category) == -1){
+        result.push(category)
+      };
+    });
+
+    return result;
+  },
 
   render: function() {
     var renderedContent = this.template({
-      atemplates: this.collection    
+      atemplates: this.collection,
+      categories: this.getCategories()    
     });
     
     this.$el.html(renderedContent);
