48a791565f369ae6617ece1de1742261d6c36f1f
Alvin Ang <alvinangbs@gmail.com>
2014-03-26 14:49:03 -0700

    Add icons and widgets for all templates

--- app/assets/javascripts/views/home/home.js
+++ app/assets/javascripts/views/home/home.js
@@ -2,6 +2,10 @@ window.Alistpress.Views.DashboardHome = Backbone.View.extend({
   
   template: JST['home/home'],
   
+  events: {
+    "click #sendEmail": "sendEmail"
+  },
+  
   addToolbar: function(){
     this.$el.find('#wysihtml5').each(function(i, elem) {
       $(elem).wysihtml5({
@@ -35,6 +39,13 @@ window.Alistpress.Views.DashboardHome = Backbone.View.extend({
     }).disableSelection();
   },  
   
+  addButtonLoading: function() {
+    this.$el.find('#sendEmail').click(function () {
+       var btn = $(this);
+       btn.button('loading');
+     });
+  },
+  
   render: function() {
     var renderedContent = this.template({
       atemplates: this.collection,
@@ -47,6 +58,31 @@ window.Alistpress.Views.DashboardHome = Backbone.View.extend({
     this.sortableIndexTodo();
     
     return this;
+  },
+  
+  sendEmail: function(event) {
+    event.preventDefault();
+    var params = $('form').serializeJSON().theme;
+    
+    this._sendEmail(params);      
+  },
+  
+  _sendEmail: function(params) {   
+    params.sent = new Boolean(true);
+    params.sender_email = Alistpress.current_user_email; 
+    params.user_id = Alistpress.current_user_id;
+    
+    var template = new Alistpress.Models.Theme(params);  
+    template.save({}, {
+      success: function(){
+        Alistpress.themes.add(template);             
+        Backbone.history.navigate("#/", { trigger: true });        
+      },
+      error: function(){
+        // need to set sent to false again..
+        alert("didn't send from home page");
+      }
+    });
   }
   
 });
--- app/assets/javascripts/views/themes/show.js
+++ app/assets/javascripts/views/themes/show.js
@@ -3,7 +3,6 @@ window.Alistpress.Views.ThemeShow = Backbone.View.extend({
   template: JST['themes/show'],
   
   render: function() {
-    debugger
     var renderedContent = this.template({
       theme: this.model
     });
